3ae56bb0091c88ce86a2e75fc5bd7ec7b24973ce
Fernando Mayorga <fernando_mabar@hotmail.com>
2025-03-20 21:04:02 +0000

    se agregaron archivos

--- repositorio3/src/app/app.module.ts
+++ repositorio3/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
